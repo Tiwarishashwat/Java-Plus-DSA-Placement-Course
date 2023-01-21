04883d8a71ef135562b72c3ae10b63ef47a5ec35
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2023-01-21 22:27:39 +0530

    Add files via upload

---
+++ Lecture-6 - Operators and Inputs/Notes/Lecture-6.md
@@ -0,0 +1,43 @@
+# Input in Java
+The Scanner class is used to get user input, and it is found in the java.util package.
+
+nextBoolean()	Reads a boolean value from the user
+nextByte()	Reads a byte value from the user
+nextDouble()	Reads a double value from the user
+nextFloat()	Reads a float value from the user
+nextInt()	Reads a int value from the user
+nextLine()	Reads a String value from the user
+nextLong()	Reads a long value from the user
+nextShort()	Reads a short value from the user
+
+Note : nextLine() reads '\n' where as other functions ignore '\n',
+and treat it as trigger to stop reading input.
+
+# Operators in Java
+There are many types of operators in Java which are given below:
+
+1. Unary Operator
+a. The Java unary operators require only one operand.
+
+2. Arithmetic Operator
+a. Java arithmetic operators are used to perform addition, subtraction, multiplication, and division. They act as basic mathematical operations.
+
+3. Shift Operator
+a. The Java left shift operator << is used to shift all of the bits in a value to the left side of a specified number of times.
+b. The Java right shift operator >> is used to move the value of the left operand to right by the number of bits specified by the right operand.
+c. For positive number, >> and >>> works same. For negative number, >>> changes parity bit (MSB) to 0.
+
+4. Relational Operator
+a. comparison and equality operators
+
+5. Bitwise Operator
+a. Bitwise operators are used to performing the manipulation of individual bits of a number.
+
+6. Logical Operator
+a. Logical operators are used to performing logical “AND”, “OR” and “NOT” operations. They are used to combine two or more conditions/constraints or to complement the evaluation of the original condition under particular consideration
+
+7. Ternary Operator
+a. Java Ternary operator is used as one line replacement for if-then-else statement and used a lot in Java programming. It is the only conditional operator which takes three operands.
+
+8. Assignment Operator
+a. Java assignment operator is one of the most common operators. It is used to assign the value on its right to the operand on its left.
---
+++ Lecture-6 - Operators and Inputs/codes/MyTemp.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class MyTemp {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println(sc instanceof Scanner);
+
+        // System.out.println("What is your name");
+        // String firstName = sc.next();
+
+        // System.out.println("What is your age");
+        // String ageInput = sc.next();
+        // int age = Integer.parseInt(ageInput);
+
+        // System.out.println("What is your address?");
+        // sc.nextLine(); //just to ignore \n
+        // String address = sc.nextLine();
+        // // sc.next
+
+        // // String name = sc.nextLine(); //'\n' 
+        // System.out.println("Hi " + firstName+ " you are " + age + " years old" );
+        // System.out.println("You live in " + address);
+
+        // int agee;
+        // Integer myAge = new 
+        
+        // unary
+        int op1 = 10;
+        // op1++;
+        // op1--;
+        int y = --op1; // 9
+        int x = ++op1; //10
+        // System.out.println(op1 +" "+ x + " "+y);
+        // op = 10;
+        int z = op1++; //z = 10 
+        // op = 11
+        int a = op1--; //a = 11
+        // op = 10
+        
+        // int op = 15;
+        // int ans = op++ + ++op - --op - op++;
+        // System.out.println(op + " " + ans);
+
+        // boolean correct = true;
+        // System.out.println(correct); 
+        // System.out.println(!correct);
+
+        // int val1 = 10; // pos -> -(val+1) -> -11
+        // System.out.println(~val1);
+
+        // System.out.println(10/3);
+        // 0110001111 >> 2
+        // 0001100011
+
+        // 0110001111 << 2
+        // 1000111100
+
+
+
+        // int val2 = -10;
+        // System.out.println(~val2); //-(val+1) 9
+
+        boolean vall1 = true;
+        boolean vall2 = true;
+        boolean vall3 = false;
+        boolean vall4 = false;
+
+        int test = 10;
+        // boolean ansss = (++test == 11);
+        // System.out.println(ansss);
+        // System.out.println( vall3 && (++test == 11) );
+        // System.out.println(test);
+
+        // System.out.println( vall1 || (++test == 11) );
+        // System.out.println(test);
+        // int res = (6>5)?(test++):(test--);
+        // System.out.println(test); //11
+        // System.out.println(res); //10
+        // System.out.println(op1 +" "+ x + " "+y+" "+ z +" "+ a);
+
+        int x11 = 5;
+
+        // x11 = x11+10;
+        // x11 += 10;
+        // x11 = x11-10;
+        // x11 -= 10;
+
+        int x22 = (27 + 40) - x11++ * 5 / 2 + 9 % 100;
+        System.out.println(x22);
+
+        // sc.close();
+    }
+}
+// shashwat    \n 22 \n
+
+
+
+
+
+
+
+
+

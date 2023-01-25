6bbc0950301e523a520077a8fdca4bd132815f70
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2023-01-25 06:03:22 +0530

    Add files via upload

---
+++ Lecture-7 - Packages and Modifiers/codes/MyStudent.java
@@ -0,0 +1,10 @@
+package com.college.student;
+public class MyStudent {
+    public static void main(String[] args) {
+        MyStudent ms = new MyStudent();
+        ms.print();
+    }
+    private void print(){
+        System.out.println("I am Student");
+    }
+}
---
+++ Lecture-7 - Packages and Modifiers/codes/StudentDetails.java
@@ -0,0 +1,6 @@
+package com.college.student.studentdetails;
+public class StudentDetails {
+    public void print(){
+        System.out.println("I am StudentDetails");
+    }
+}

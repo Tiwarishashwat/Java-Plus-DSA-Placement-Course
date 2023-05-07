c47d894826d08ba502bbe3dfb0ba950cd7316e65
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2023-05-07 09:31:42 +0530

    Delete Lecture-7 - Packages and Modifiers directory

--- Lecture-7 - Packages and Modifiers/codes/StudentDetails.java
+++
@@ -1,6 +0,0 @@
-package com.college.student.studentdetails;
-public class StudentDetails {
-    public void print(){
-        System.out.println("I am StudentDetails");
-    }
-}
--- Lecture-7 - Packages and Modifiers/codes/TeacherDetails.java
+++
@@ -1,6 +0,0 @@
-package com.college.teacher.teacherdetails;
-public class TeacherDetails {
-    public void print(){
-        System.out.println("I am TeacherDetails");
-    }
-}

256278c1e570e16a72218fcd3e6a7070dbfe4463
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2024-11-15 14:22:13 +0530

    Update BinarySearch4.java

--- Lecture-022 - Rotated Sorted Array/code/BinarySearch4.java
+++ Lecture-022 - Rotated Sorted Array/code/BinarySearch4.java
@@ -1,29 +1,6 @@
-public class BinarySearch4 {
-    public static void main(String args[]){
-        int arr[] = {2,4,5,7,11,43};
-        int index = minElement(arr);
-        System.out.println(arr[index]);
-        int index1 = maxElement(arr);
-        System.out.println(arr[index1]);
-        int index2 = searchInRotatedArray(arr,66);
-        if(index2 == -1){
-            System.out.println("Element does not exist");
-        } else
-        System.out.println(arr[index2]);
-        rotationCountInCWSA(arr);
-        rotationCountInACWSA(arr);
-
-    }
-
-    static void rotationCountInACWSA(int arr[]){
-        int index = minElement(arr);
-        System.out.println("ACW Rotation count is:"+ ((arr.length-index)%arr.length));
-    }
-    static void rotationCountInCWSA(int arr[]){
-        int index = minElement(arr);
-        System.out.println("CW Rotation count is:"+ index);
-    }
-    static int searchInRotatedArray(int arr[], int target){
+//leetcode 33. Search in Rotated Sorted Array
+class Solution {
+    public int search(int[] arr, int target) {
         int start = 0;
         int end = arr.length-1;
         int ans=-1;
@@ -40,7 +17,7 @@ public class BinarySearch4 {
                 } else {
                     start = mid + 1;
                 }
-            } // right side is sorted
+            } // right side is sorted 
             else {
                 // can ans be found in right side?
                 if (target > arr[mid] && target <= arr[end]) {
@@ -52,34 +29,122 @@ public class BinarySearch4 {
         }
         return ans;
     }
-    static int maxElement(int arr[]){
+}
+
+// leetcode 81. Search in rotated sorted array 2
+class Solution {
+    public boolean search(int[] arr, int target) {
         int start = 0;
         int end = arr.length-1;
-        while (start < end){
-            int mid = start + (end-start)/2;
-            if(mid<arr.length-1 && arr[mid]>arr[mid+1]){
-                return mid;
+        boolean ans=false;
+        // tc: logN + O(N/2)
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return true;
             }
-            // if right half is sorted, move left
-            else if(arr[start]>arr[mid]){
-                end = mid-1;
-            }   // if left half is sorted, move right
+            // if start,mid and end all are same then we can't guarentee which
+            // part is sorted so we will trim down the space
+            if(arr[start] == arr[mid] && arr[mid] == arr[end]){
+                start++;
+                end--;
+            }
+            // left side is sorted
+            else if (arr[start] <= arr[mid]) {
+                // can ans be found in left side?
+                if (target >= arr[start] && target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } // right side is sorted 
             else {
-                start = mid+1;
+                // can ans be found in right side?
+                if (target > arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
-        return end;
+        return ans;
     }
-    static int minElement(int arr[]){
+}
+
+// leetcode 153. Find Minimum in Rotated Sorted Array
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        int minElement = Integer.MAX_VALUE;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[start]<=nums[mid]){
+                minElement = Math.min(minElement,nums[start]);
+                start = mid+1;
+            }else{
+                minElement = Math.min(minElement,nums[mid]);
+                end = mid-1;
+            }
+        }
+        return minElement;
+    }
+}
+
+// gfg find Kth Rotation
+
+class Solution {
+    public int findKRotation(List<Integer> arr) {
+        // Code here
+        int n = arr.size();
+        int start = 0;
+        int end = n-1;
+        int minElement = Integer.MAX_VALUE;
+        int minIndex = Integer.MAX_VALUE;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr.get(start)<=arr.get(mid)){
+                // minElement = Math.min(minElement,nums[start]);
+                if(arr.get(start) < minElement){
+                    minElement = arr.get(start);
+                    minIndex = start;
+                }
+                start = mid+1;
+            }else{
+                // minElement = Math.min(minElement,nums[mid]);
+                if(arr.get(mid) < minElement){
+                    minElement = arr.get(mid);
+                    minIndex = mid;
+                }
+                end = mid-1;
+            }
+        }
+        return minIndex;
+    }
+}
+
+// Homework
+// 154. Find Minimum in Rotated Sorted Array II
+class Solution {
+    public int findMin(int[] nums) {
+        int index = minElement(nums);
+       return nums[index];
+    }
+      static int minElement(int arr[]){
     int start = 0;
     int end = arr.length-1;
     while (start < end){
         int mid = start + (end-start)/2;
-        if(mid>0 && arr[mid]<arr[mid-1]){
+        if(arr[start]==arr[mid] && arr[mid]==arr[end]){
+            start++;
+            end--;
+        }
+        else if(mid>0 && arr[mid]<arr[mid-1]){
             return mid;
         }
         // if right half is sorted, move left
-        else if(arr[end]>arr[mid]){
+        else if(arr[end]>=arr[mid]){
             end = mid-1;
         }   // if left half is sorted, move right
         else {
@@ -88,35 +153,4 @@ public class BinarySearch4 {
     }
     return start;
     }
-    static int minElementDuplicates(int arr[]){
-        int start = 0;
-        int end = arr.length-1;
-        while (start < end){
-            int mid = start + (end-start)/2;
-            if(arr[start]==arr[mid] && arr[mid]==arr[end]){
-                start++;
-                end--;
-            }
-            else if(mid>0 && arr[mid]<arr[mid-1]){
-                return mid;
-            }
-            // if right half is sorted, move left
-            else if(arr[end]>=arr[mid]){
-                end = mid-1;
-            }   // if left half is sorted, move right
-            else {
-                start = mid+1;
-            }
-        }
-        return start;
-    }
 }
-
-
-
-
-
-
-
-
-

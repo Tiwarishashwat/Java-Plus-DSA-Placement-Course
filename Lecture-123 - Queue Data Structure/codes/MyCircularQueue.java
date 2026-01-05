92bcfc7b18357412fc5b7071474c8b173d964de4
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2026-01-05 19:56:29 +0530

    Refactor MyCircularQueue for improved readability

--- Lecture-123 - Queue Data Structure/codes/MyCircularQueue.java
+++ Lecture-123 - Queue Data Structure/codes/MyCircularQueue.java
@@ -5,66 +5,55 @@ public class MyCircularQueue {
     int size;
     int capacity;
     int circularQueue[];
-
     public MyCircularQueue(int capacity) {
         circularQueue = new int[capacity];
         this.capacity = capacity;
-        size = 0;
-        rear = front = -1;
+        size=0;
+        rear = front =-1;
     }
 
     public boolean isEmpty() {
-        if (front == -1) {
-            return true;
-        }
-        return false;
+       return (size==0);
     }
 
     public boolean isFull() {
-        if ((front==0 && rear == capacity - 1) ||(front!=0 && rear==(front-1)%(capacity))) {
-            return true;
-        }
-        return false;
+       return (size==capacity);
     }
 
     public int peek() {
-        if (isEmpty()) {
-            System.out.println("circularQueue is empty");
+        if(isEmpty()){
+            System.out.println("Queue is empty");
             return -1;
         }
-        return circularQueue[front];
+        return circularQueue[front%capacity];
     }
 
     public void enqueue(int val) {
-        if (isFull()) {
-            System.out.println("circularQueue is full");
+        if(isFull()){
+            System.out.println("Queue is full");
             return;
         }
 
-        if (isEmpty()) {
+        if(isEmpty()) {
             front = 0;
             rear = 0;
-        } else if(rear==capacity-1 && front!=0) { //circular insertion
-            rear=0;
-        } else{ // normal case
+        } else {
             rear++;
         }
         size++;
-        circularQueue[rear] = val;
+        circularQueue[rear%capacity] = val;
     }
 
-    public int dequeue() {
-        if (isEmpty()) {
-            System.out.println("circularQueue is empty");
+    public int dequeue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
             return -1;
         }
-        int val = circularQueue[front];
-        if (front == rear) {
-            front = -1;
-            rear = -1;
-        } else if(front==capacity-1){ // circular deletion
-            front=0;
-        } else { // normal
+        int val = circularQueue[front%capacity];
+        if(front==rear){
+            front=-1;
+            rear=-1;
+        } else {
             front++;
         }
         size--;
@@ -75,103 +64,74 @@ public class MyCircularQueue {
         return size;
     }
 
-    public void printQueue() {
-        if (isEmpty()) {
-            System.out.println("circularQueue is empty");
+    public void printQueue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
             return;
         }
-        // circular case
-        if(rear<=front){
-            for (int i = front; i <= capacity-1; i++) {
-                System.out.print(circularQueue[i]);
+        for(int i=front;i<=rear;i++){
+            System.out.print(circularQueue[i%capacity]);
+            if(i!=rear){
                 System.out.print(",");
             }
-            for (int i = 0; i <= rear; i++) {
-                System.out.print(circularQueue[i]);
-                if (i != rear) {
-                    System.out.print(",");
-                }
-            }
-        } else { // normal printing
-            for (int i = front; i <= rear; i++) {
-                System.out.print(circularQueue[i]);
-                if (i != rear) {
-                    System.out.print(",");
-                }
-            }
         }
-
         System.out.println();
-
-
     }
 
     public static void main(String[] args) {
 
-        MyCircularQueue myCircularQueue = new MyCircularQueue(5);
-        System.out.println("Capacity is: " + 5);
+        MyCircularQueue myQueue = new MyCircularQueue(5);
+        System.out.println("Capacity is: "+5);
         System.out.println("Print:");
-        myCircularQueue.printQueue();
-        System.out.println("Insert element :" + 7);
-        myCircularQueue.enqueue(7);
-        System.out.println("Peak element: " + myCircularQueue.peek());
-        System.out.println("Insert element :" + 8);
-        myCircularQueue.enqueue(8);
-        System.out.println("Peak element: " + myCircularQueue.peek());
-        System.out.println("Insert element :" + 9);
-        myCircularQueue.enqueue(9);
-        System.out.println("Peak element: " + myCircularQueue.peek());
-        System.out.println("Insert element :" + 11);
-        myCircularQueue.enqueue(11);
-        System.out.println("Insert element :" + 19);
-        myCircularQueue.enqueue(19);
-        System.out.println("Insert element :" + 9);
-        myCircularQueue.enqueue(9);
-        System.out.println("is CircularQueue full: " + myCircularQueue.isFull());
+        myQueue.printQueue();
+        System.out.println("Insert element :"+7);
+        myQueue.enqueue(7);
+        System.out.println("Peak element: "+ myQueue.peek());
+        System.out.println("Insert element :"+8);
+        myQueue.enqueue(8);
+        System.out.println("Peak element: "+ myQueue.peek());
+        System.out.println("Insert element :"+9);
+        myQueue.enqueue(9);
+        System.out.println("Peak element: "+ myQueue.peek());
+        System.out.println("Insert element :"+11);
+        myQueue.enqueue(11);
+        System.out.println("Insert element :"+19);
+        myQueue.enqueue(19);
+        System.out.println("Insert element :"+9);
+        myQueue.enqueue(9);
+        System.out.println("is Queue full: "+ myQueue.isFull());
         System.out.println("Print:");
-        myCircularQueue.printQueue();
+        myQueue.printQueue();
         System.out.println("delete element");
-        myCircularQueue.dequeue();
-        System.out.println("is CircularQueue full: " + myCircularQueue.isFull());
+        myQueue.dequeue();
+        System.out.println("is Queue full: "+ myQueue.isFull());
         System.out.println("Print:");
-        myCircularQueue.printQueue();
-        System.out.println("Insert element :" + 19);
-        myCircularQueue.enqueue(19);
+        myQueue.printQueue();
+        System.out.println("Insert element :"+19);
+        myQueue.enqueue(19);
         System.out.println("Print:");
-        myCircularQueue.printQueue();
-        System.out.println("Peak element: " + myCircularQueue.peek());
+        myQueue.printQueue();
+        System.out.println("Peak element: "+ myQueue.peek());
         System.out.println("delete element");
-        myCircularQueue.dequeue();
-        System.out.println("Peak element: " + myCircularQueue.peek());
+        myQueue.dequeue();
+        System.out.println("Peak element: "+ myQueue.peek());
 
-        System.out.println("is CircularQueue empty: " + myCircularQueue.isEmpty());
+        System.out.println("is Queue empty: "+ myQueue.isEmpty());
 
         System.out.println("Print:");
-        myCircularQueue.printQueue();
+        myQueue.printQueue();
         System.out.println("delete element");
-        myCircularQueue.dequeue();
+        myQueue.dequeue();
         System.out.println("delete element");
-        myCircularQueue.dequeue();
+        myQueue.dequeue();
         System.out.println("delete element");
-        myCircularQueue.dequeue();
+        myQueue.dequeue();
         System.out.println("delete element");
-        myCircularQueue.dequeue();
-        System.out.println("is CircularQueue empty: " + myCircularQueue.isEmpty());
+        myQueue.dequeue();
+        System.out.println("is Queue empty: "+ myQueue.isEmpty());
         System.out.println("Print:");
-        myCircularQueue.printQueue();
+        myQueue.printQueue();
         System.out.println("delete element");
-        myCircularQueue.dequeue();
+        myQueue.dequeue();
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
